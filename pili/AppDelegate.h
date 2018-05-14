d9ee7dd6c5430a3a28861bf58fa7ba3660901ca5
ejoy <fengzhiyun21@163.com>
2018-05-15 00:54:06 +0800

    初始化

---
+++ pili/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pili
+//
+//  Created by ejoy on 2018/5/15.
+//  Copyright © 2018年 ejoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pili
+//
+//  Created by ejoy on 2018/5/15.
+//  Copyright © 2018年 ejoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
